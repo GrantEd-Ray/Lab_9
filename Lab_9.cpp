ee197dd49663132ae4c14c515e2e9a6990a441ce
GrantEd-Ray <gridashkov@gmail.com>
2022-11-04 21:19:59 +0200

    New

---
+++ Lab_9.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+
+class Rectangle
+{
+private:
+    double rect_lenght = 0;
+    double rect_width = 0;
+    double rect_area = 0;
+public:
+    Rectangle(double length, double width)
+    {
+        rect_lenght = length;
+        rect_width = width;
+    }
+
+    ~Rectangle() {}
+
+    double Area()
+    {
+        rect_area = rect_lenght * rect_width;
+        return rect_area;
+    }
+};
+
+
+int main()
+{
+    double a;
+    double b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    Rectangle rect_1(a, b);
+
+    std::cout << "Area of rectangle is " << rect_1.Area() << std::endl;
+}
